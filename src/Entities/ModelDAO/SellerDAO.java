e8abfa2b0128c6f972b7b751c68e34827f3fc359
David Alves Dutra <dadutra@hotmail.com>
2024-06-20 18:36:46 -0300

    Department and Seller interfaces (DAO) added.

---
+++ src/Entities/ModelDAO/SellerDAO.java
@@ -0,0 +1,16 @@
+package Entities.ModelDAO;
+import Entities.Seller;
+
+import java.util.List;
+
+public interface SellerDAO {
+    void insert(Seller seller);
+
+    void update(Seller seller);
+
+    void deleteById(Integer id);
+
+    Seller findById(Integer id);
+
+    List<Seller> findAll();
+}
