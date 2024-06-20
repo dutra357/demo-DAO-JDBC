fbe4508a56830c4f43b5cccc29add14f61124444
David Alves Dutra <dadutra@hotmail.com>
2024-06-20 19:12:19 -0300

    FindById implementation, with SQL query.

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,22 +1,24 @@
 package Application;
-
 import Entities.Department;
 import Entities.FactoryDAO;
 import Entities.ModelDAO.SellerDAO;
 import Entities.Seller;
-
+import java.io.IOException;
 import java.util.Date;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Department department = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
 
+
         SellerDAO sellerDAO = FactoryDAO.createSellerDao();
 
 
+        Seller seller1 = sellerDAO.findById(3);
+
         System.out.println(department);
-        System.out.println(seller);
+        System.out.println(seller1);
     }
 }
--- src/Entities/FactoryDAO.java
+++ src/Entities/FactoryDAO.java
@@ -1,10 +1,13 @@
 package Entities;
+import DataBase.DataBase;
 import Entities.ImplementDAO.SellerDaoJDBC;
 import Entities.ModelDAO.SellerDAO;
 
+import java.io.IOException;
+
 public class FactoryDAO {
 
-    public static SellerDAO createSellerDao() {
-        return new SellerDaoJDBC();
+    public static SellerDAO createSellerDao() throws IOException {
+        return new SellerDaoJDBC(DataBase.getConnection());
     }
 }
